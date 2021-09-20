be000a5bd59575d76e9f6e1e9b046eae47f5b2ee
Sajana Kavinda <49279952+sajjkavinda@users.noreply.github.com>
2021-09-20 11:26:52 +0530

    changed paths and added platform and time to the log file

--- platformSOS.py
+++ platformSOS.py
@@ -2,18 +2,21 @@ import platform
 import os
 import datetime
 
-print("Platform: " + platform.platform())
+plat = "Platform: " + platform.platform()
 
 a = platform.system()
 if (a == "Linux"):
     x = datetime.datetime.now()
-    print(x.strftime("%c"))
+    timeL  = x.strftime("%c")
+    print (timeL)
 
-    f1 = open(r'/Desktop/entries_Lin.log', 'w') 
+    f1 = open(r'root/Desktop/entries_Lin.log', 'w')
 
     os.chdir(r'/lib')
-    
-    for root, dirs, files in os.walk("/lib"):
+
+    f1.write(plat + "\n" + timeL + "\n")
+
+    for root, dirs, files in os.walk("/usr/lib"):
         for file in files:
             if file.endswith(".bin"):
                 print(file)
@@ -22,12 +25,15 @@ if (a == "Linux"):
 elif (a == "Windows"):
 
     x = datetime.datetime.now()
-    print(x.strftime("%d" "-" "%b" "-" "%y"))
-    print(x.strftime("%I" ":" "%M" " " "%p"))
+    timeW1 = x.strftime("%d" "-" "%b" "-" "%y")
+    timeW2 = x.strftime("%I" ":" "%M" " " "%p")
+    print(timeW1  + "\n" + timeW2)
 
-    f2 = open(r'C:\entries_Win10.log', 'w') 
+    f2 = open(r'C:\Desktop\entries_Win10.log', 'w') 
 
     os.chdir(r'C:\Windows\system32')
+
+    f2.write(plat + "\n" + timeW1 + "\n" + timeW2 + "\n" )
 
     for root, dirs, files in os.walk("C:\Windows\system32"):
         for file in files:
@@ -38,12 +44,15 @@ elif (a == "Windows"):
 elif (a == "Darwin"):
 
     x = datetime.datetime.now()
-    print(x.strftime("%d" "-" "%b" "-" "%y"))
-    print(x.strftime("%I" ":" "%M" " " "%p")) 
+    mactime = x.strftime("%d" "-" "%b" "-" "%y")
+    mactime2 = x.strftime("%I" ":" "%M" " " "%p")
+    print(mactime + "\n" + mactime2)
 
     f3 = open(r'/Users/sajjkavinda/entries_mac.log', 'w') 
 
     os.chdir(r'/Users/sajjkavinda/Downloads/')
+
+    f3.write(plat + "\n" + mactime + "\n" + mactime2 + "\n" )
 
     for root, dirs, files in os.walk('/Users/sajjkavinda/Downloads/'):
         for file in files:
