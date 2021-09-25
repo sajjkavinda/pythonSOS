ef6a90792e0a7a3280fde3687354ca08c7660b66
Sajana Kavinda <49279952+sajjkavinda@users.noreply.github.com>
2021-09-25 20:33:25 +0530

    finalized

--- platformSOS.py
+++ platformSOS.py
@@ -1,43 +1,49 @@
+# K.G.S.K. Kirindeniya | IT20614058 | Y2S1 CS
+
 import platform, os
 import datetime, time
 
+#platform information
 plat = "Platform: " + platform.platform() + "\n"
 print (plat)
 
-a = platform.system()
-if (a == "Linux"):
+a = platform.system() #platform type extract
+
+if (a == "Linux"): #check the platform type
+
+    #initialize , sort and print the system time
     x = datetime.datetime.now()
-    timeL  = x.strftime("%a %b %m %H:%M:%S")
-    timezone = time.timezone
-    year = x.strftime("%Y")
-    print (timeL + " ", timezone , " " , year , "\n")
+    timeL  = x.strftime("%a %b %m %H:%M:%S" , str(time.timezone), "%Y")
+    #timezone = time.strftime(str(time.timezone))
+    #year = x.strftime("%Y")
+    print (timeL + "\n")
 
-    f1 = open(r'root/Desktop/entries_Lin.log', 'w')
+    f1 = open(os.path.expanduser("~/Desktop/Entries_Lin.log"), "w") #create a log file to write details
 
-    os.chdir(r'/lib')
+    f1.write(plat + "\n" + timeL + "\n \n") #write the platform date and time details in the log file
 
-    f1.write(plat + "\n" + timeL + "\n")
-
-    for root, dirs, files in os.walk("/usr/lib"):
+    #access folders and subfolders in /usr/lib path -> print in the shell and write into the log file
+    for root, dirs, files in os.walk("/usr/lib/"):
         for file in files:
-            if file.endswith(".bin"):
-                print(os.getcwd() + (file))
-                f1.write(os.getcwd() + file + "\n")
+            if file.endswith(".bin"): 
+                print(os.getcwd() + ("/" + file)) 
+                f1.write(os.getcwd() + "/" + file + "\n")
 
-elif (a == "Windows"):
+elif (a == "Windows"): #check the platform type
+
+    #initialize , sort and print the system time
     x = datetime.datetime.now()
     timeW1 = x.strftime("%d" "-" "%b" "-" "%y")
     timeW2 = x.strftime("%I" ":" "%M" " " "%p")
-    print(timeW1  + "\n" + timeW2 +"\n" + "\n Directries of C:\Windows\system32 \n")
+    print(timeW1 + "\n" + timeW2 + "\n")
 
-    f2 = open(r'C:\Users\user\Desktop\entries_Win10.log', 'w')
+    f2 = open(os.path.expanduser("~\Desktop\Entries_Win10.log"), "w") #create a log file to write details
 
-    os.chdir(r'C:\Windows\system32')
+    f2.write(plat + "\n" + timeW1 + "\n" + timeW2 + "\n") #write the platform, date and time details in the log file
 
-    f2.write(plat + "\n" + timeW1 + "\n" + timeW2 + "\n" + "Directries of C:\Windows\system32 \n" + "\n")
+    details = os.popen('cmd /k "dir C:\\Windows\system32\*.exe"') #run the shell command for sort out .exe files
 
-    details = os.popen('cmd /k "dir C:\\Windows\system32\*.exe"')
-
+    #print and redirect the shell output
     for records in details:
         print(details)
         f2.write(details)
