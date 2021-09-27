f5c0d36a7e39dfb40455a5eec25db99d1f477873
Sajana Kavinda <49279952+sajjkavinda@users.noreply.github.com>
2021-09-27 19:26:18 +0530

    removed shell print

--- platformSOS.py
+++ platformSOS.py
@@ -11,23 +11,23 @@ a = platform.system() #platform type extract
 
 if (a == "Linux"): #check the platform type
 
-    #initialize , sort and print the system time
+    #initialize , sort and print the system time and year
     x = datetime.datetime.now()
-    timeL  = x.strftime("%a %b %m %H:%M:%S" , str(time.timezone), "%Y")
-    #timezone = time.strftime(str(time.timezone))
-    #year = x.strftime("%Y")
-    print (timeL + "\n")
+    timeL  = x.strftime("%a %b %m %H:%M:%S")
+    timezone = time.strftime(str(time.timezone))
+    year = x.strftime("%Y")
 
     f1 = open(os.path.expanduser("~/Desktop/Entries_Lin.log"), "w") #create a log file to write details
 
-    f1.write(plat + "\n" + timeL + "\n \n") #write the platform date and time details in the log file
-
+    f1.write(plat + "\n" + timeL + timezone + year + "\n \n") #write the platform date and time details in the log file
+    
+    os.chdir(r"/usr/lib/")
+    
     #access folders and subfolders in /usr/lib path -> print in the shell and write into the log file
-    for root, dirs, files in os.walk("/usr/lib/"):
-        for file in files:
-            if file.endswith(".bin"): 
-                print(os.getcwd() + ("/" + file)) 
-                f1.write(os.getcwd() + "/" + file + "\n")
+    details = os.popen('sudo find /lib/ -name "*.bin" -type f')
+    
+    for records in details:
+        f1.write(records)
 
 elif (a == "Windows"): #check the platform type
 
@@ -35,7 +35,6 @@ elif (a == "Windows"): #check the platform type
     x = datetime.datetime.now()
     timeW1 = x.strftime("%d" "-" "%b" "-" "%y")
     timeW2 = x.strftime("%I" ":" "%M" " " "%p")
-    print(timeW1 + "\n" + timeW2 + "\n")
 
     f2 = open(os.path.expanduser("~\Desktop\Entries_Win10.log"), "w") #create a log file to write details
 
@@ -45,5 +44,4 @@ elif (a == "Windows"): #check the platform type
 
     #print and redirect the shell output
     for records in details:
-        print(records)
         f2.write(records)
